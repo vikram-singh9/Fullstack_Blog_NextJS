12d5762468d41698374566bfefadeb7f4e7deddf
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-04-07 00:00:05 +0500

    Update page.tsx

--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const home = () => {
   return (
-    <div className='flex justify-center items-center'>home</div>
+    <div className='flex items-center justify-center h-screen'>home</div>
   )
 }
 
