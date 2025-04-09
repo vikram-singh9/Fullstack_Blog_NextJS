6c730b4004fe0ef0d5c0a1291e66ecc01deeea06
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-04-10 00:46:59 +0500

    updated

--- blog/app/components/navbar.tsx
+++ blog/app/components/navbar.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Link from 'next/link'
-import About from '../about/page'
+
 
 const Navbar = () => {
   return (
-    <div>Navbar
+    <div className='flex w-full items-center justify-around px-9 py-5 text-blue-500 '>
+      <div>
+        <h1>BlogsMania</h1>
+      </div>
+
         <Link href={"/about"}><h1>About</h1></Link>
+        <Link href={"/blogs"}><h1>Blogs</h1></Link>
+        <Link href={"/contacts"}><h1>Contacts</h1></Link>
+        <Link href={"/pricing"}><h1>Pricing</h1></Link>
     </div>
   )
 }
